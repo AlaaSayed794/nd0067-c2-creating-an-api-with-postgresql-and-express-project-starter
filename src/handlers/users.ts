dbb8a09c8326eaf3d0a65a09a9eac198237c1991
AlaaSayed794 <alaa.elsayed.aly@gmail.com>
2021-10-27 23:30:21 +0200

    adding salt and pepper and authentication method to users

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -4,41 +4,47 @@ import { User, UserStore } from '../models/user';
 const store = new UserStore();
 
 const index = async (_req: Request, res: Response) => {
-  const users = await store.index();
-  res.json(users);
+    const users = await store.index();
+    res.json(users);
 };
 
 const show = async (req: Request, res: Response) => {
-  const user = await store.show(req.params.id);
-  res.json(user);
+    const user = await store.show(req.params.id);
+    res.json(user);
 };
 
 const create = async (req: Request, res: Response) => {
-  try {
-    const user: User = {
-      email: req.body.email,
-      user_name: req.body.user_name,
-      password: req.body.password
-    };
+    try {
+        const user: User = {
+            email: req.body.email,
+            user_name: req.body.user_name,
+            password: req.body.password
+        };
 
-    const newUser = await store.create(user);
-    res.json(newUser);
-  } catch (err) {
-    res.status(400);
-    res.json(err);
-  }
+        const newUser = await store.create(user);
+        res.json(newUser);
+    } catch (err) {
+        res.status(400);
+        res.json(err);
+    }
 };
 
 const destroy = async (req: Request, res: Response) => {
-  const deleted = await store.delete(req.body.id);
-  res.json(deleted);
+    const deleted = await store.delete(req.body.id);
+    res.json(deleted);
+};
+
+const authenticate = async (req: Request, res: Response) => {
+    const user = await store.authenticate(req.body.user_name, req.body.password);
+    res.json(user);
 };
 
 const userRoutes = (app: express.Application): void => {
-  app.get('/users', index);
-  app.get('/users/:id', show);
-  app.post('/users', create);
-  app.delete('/users', destroy);
+    app.get('/users', index);
+    app.get('/users/:id', show);
+    app.post('/users', create);
+    app.delete('/users', destroy);
+    app.post('/authenticate', authenticate)
 };
 
 export default userRoutes;
